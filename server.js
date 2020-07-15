2bafb4ae0ec73f4206e59fbb559eea2d17be626e
Junaid Farooq <junaid@evercam.io>
2020-07-15 21:45:35 +0500

    Added auth post endpoint && return token on authenticating user.

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express()
 app.use(express.json({ extended: false }))
 
 app.use("/api/users", require("./routes/users"))
-// app.use("/api/auth", require("./routes/auth"))
+app.use("/api/auth", require("./routes/auth"))
 // app.use("/api/contacts", require("./routes/contacts"))
 
 const PORT = process.env.PORT || 5000
