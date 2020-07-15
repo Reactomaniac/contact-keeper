1f1d6ccf834b76c94cdd70fe621146fe70bae396
Junaid Farooq <junaid@evercam.io>
2020-07-15 19:45:07 +0500

    Connected DB && passed express.json.

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require("express")
+const connectDB = require("./config/db")
+
+connectDB()
 
 const app = express()
+
+app.use(express.json({ extended: false }))
+
+app.use("/api/users", require("./routes/users"))
+// app.use("/api/auth", require("./routes/auth"))
+// app.use("/api/contacts", require("./routes/contacts"))
 
 const PORT = process.env.PORT || 5000
 
