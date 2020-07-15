19dc526eb7d47e5ba1e7a860adbf9f224d2552b4
Junaid Farooq <junaid@evercam.io>
2020-07-15 21:58:58 +0500

    Used Auth middleware && Added get auth.

--- routes/auth.js
+++ routes/auth.js
@@ -4,8 +4,19 @@ const router = express.Router()
 const config = require("config")
 const jwt = require("jsonwebtoken")
 const { check, validationResult } = require("express-validator")
+const auth = require("../middleware/auth")
 
 const User = require("../models/User")
+
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password")
+    res.json(user)
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send("Server Error")
+  }
+})
 
 router.post(
   "/",
