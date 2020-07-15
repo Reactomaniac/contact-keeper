9035df806ba4de108b2db79d940e4d0485bb34ac
Junaid Farooq <junaid@evercam.io>
2020-07-15 17:43:28 +0500

    Connected to MongoDB.

---
+++ config/db.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose")
+const config = require("config")
+
+const db = config.get("mongoURI")
+
+const connectDB = async () => {
+
+  try {
+    mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    })
+    console.log("Trying to connect to MongoDB")
+  } catch (err) {
+    console.error(err.message)
+    process.exit(1)
+  } finally {
+    console.log("MongoDB connected..")
+  }
+}
+
+module.exports = connectDB
