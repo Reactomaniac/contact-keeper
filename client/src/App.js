275c41d461e09175b43e08c91dc7f1d8945b6444
Junaid Farooq <junaid@evercam.io>
2020-07-16 18:00:33 +0500

    Added a default Home.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import './App.css';
+import Navbar from "./components/layout/Navbar"
+import Home from "./components/pages/Home"
 
 const App = () => {
   return (
-    <div className="App">My App</div>
-  );
+    <Router>
+      <Fragment>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Home} />
+          </Switch>
+        </div>
+      </Fragment>
+    </Router>
+  )
 }
 
 export default App;
