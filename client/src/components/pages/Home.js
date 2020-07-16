844a23a202095b585d9b31933f97c4e3a742c433
Junaid Farooq <junaid@evercam.io>
2020-07-16 21:14:00 +0500

    Added Contact Component.

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,8 +1,14 @@
 import React from "react"
+import Contacts from "../contacts/Contacts"
 
 const Home = () => {
   return (
-    <div>HOME</div>
+    <div className="grid-2">
+      <div>{/* ContactForm */}</div>
+      <div>
+        <Contacts />
+      </div>
+    </div>
   )
 }
 
