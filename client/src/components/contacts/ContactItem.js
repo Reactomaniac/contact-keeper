5bddc3ae120982442622eabf25ee31fca920809c
Junaid Farooq <junaid@evercam.io>
2020-07-16 21:51:22 +0500

    Fixed typo in ContactItem.(Char)

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -16,7 +16,7 @@ const ContactItem = ({ contact }) => {
             (type === "professional" ? "badge-success" : "badge-primary")
           }
         >
-          {type.chartAt(0).toUpperCase() + type.slice(1)}
+          {type.charAt(0).toUpperCase() + type.slice(1)}
         </span>
       </h3>
       <ul className="list">
